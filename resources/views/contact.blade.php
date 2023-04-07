9de9be3aa89bfbd195b59ec15b89c07aebb45bcc
Son <bhoangson0410@gmail.com>
2023-04-07 14:56:34 +0700

    Check

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -58,8 +58,8 @@ https://templatemo.com/tm-571-hexashop
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
                             <li class="scroll-to-section"><a href="{{url('/')}}" class="active">Home</a></li>
-                            <li class="scroll-to-section"><a href="{{url('/')}}">Men's</a></li>
-                            <li class="scroll-to-section"><a href="{{url('/')}}">Women's</a></li>
+                            <li class="scroll-to-section"><a href="{{url('/')}}"n>Men's</a></li>
+                            <li class="scroll-to-section"><a href="{{url('/')}}">Wome's</a></li>
                             <li class="scroll-to-section"><a href="{{url('/')}}">Kid's</a></li>
                             <li class="submenu">
                                 <a href="javascript:;">Pages</a>
@@ -67,15 +67,15 @@ https://templatemo.com/tm-571-hexashop
                                     <li><a href="{{url('/about')}}">About Us</a></li>
                                     <li><a href="{{url('/products')}}">Products</a></li>
                                     <li><a href="{{url('/single-product')}}">Single Product</a></li>
-                                    <li><a href="{{url('/contact')}}">Contact Ussss</a></li>
+                                    <li><a href="{{url('/contact')}}">Contact Usssss</a></li>
                                 </ul>
                             </li>
                             <li class="submenu">
                                 <a href="javascript:;">Features</a>
                                 <ul>
-                                    <li><a href="#">Features Page 1</a></li>
-                                    <li><a href="#">Features Page 2</a></li>
-                                    <li><a href="#">Features Page 3</a></li>
+                                    <li><a href="#">Features Page 11</a></li>
+                                    <li><a href="#">Features Page 21</a></li>
+                                    <li><a href="#">Features Page 31</a></li>
                                     <li><a rel="nofollow" href="https://templatemo.com/page/4" target="_blank">Template Page 4</a></li>
                                 </ul>
                             </li>
